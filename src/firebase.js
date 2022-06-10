83d550ae7b175c733ff501b96cd6d220d63b7f4b
Luis Arista <larista93@hotmail.com>
2022-06-09 22:53:17 -0500

    correccion de errores

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import env from 'react-dotenv';
 
 const firebaseConfig = {
     apiKey: env.API_KEY_FIREBASE,
-    authDomain: env.AUTH_DOMAIN_FIREBASE,
-    projectId: env.PROJECT_ID_FIRE,
-    storageBucket: env.STORAGE_BUCKET_FIRE,
-    messagingSenderId: env.MESSAGING_SENDER_ID,
-    appId: env.APP_ID_FIRE
+    authDomain: "prueba-crud-react-ee4bb.firebaseapp.com",
+    projectId: "prueba-crud-react-ee4bb",
+    storageBucket: "prueba-crud-react-ee4bb.appspot.com",
+    messagingSenderId: "183086234239",
+    appId: "1:183086234239:web:ac7feaeadb93a6c8445d4a"
   };
 
   const firebaseApp = initializeApp(firebaseConfig)
